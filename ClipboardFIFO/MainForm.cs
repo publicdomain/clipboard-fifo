788e279201a5e12d6dd2379d8aaa9086b2e4efe0
Victor L. Senior <publicdomainvic@gmail.com>
2020-06-20 06:10:45 -0400

    Add initial project files

---
+++ ClipboardFIFO/MainForm.cs
@@ -0,0 +1,231 @@
+﻿// <copyright file="MainForm.cs" company="PublicDomain.com">
+//     CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication
+//     https://creativecommons.org/publicdomain/zero/1.0/legalcode
+// </copyright>
+
+namespace ClipboardFIFO
+{
+    // Directives
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Runtime.InteropServices;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// The clipboard update windows message.
+        /// </summary>
+        private const int WmClipboardUpdate = 0x031D;
+
+        /// <summary>
+        /// The mod control.
+        /// </summary>
+        private const int MODCONTROL = 0x0002; // Changed from MOD_CONTROL for StyleCop
+
+        /// <summary>
+        /// The wm hotkey.
+        /// </summary>
+        private const int WMHOTKEY = 0x0312; // Changed from  for StyleCop
+
+        /// <summary>
+        /// The copy count.
+        /// </summary>
+        private int copyCount = 0;
+
+        /// <summary>
+        /// The associated icon.
+        /// </summary>
+        private Icon associatedIcon = null;
+
+        /// <summary>
+        /// The settings data.
+        /// </summary>
+        private SettingsData settingsData = new SettingsData();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ClipboardFIFO.MainForm"/> class.
+        /// </summary>
+        public MainForm()
+        {
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            this.InitializeComponent();
+
+            // Add clipboard listener
+            AddClipboardFormatListener(this.Handle);
+        }
+
+        /// <summary>
+        /// Adds the clipboard format listener.
+        /// </summary>
+        /// <returns><c>true</c>, if clipboard format listener was added, <c>false</c> otherwise.</returns>
+        /// <param name="hwnd">The handle.</param>
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        /// <summary>
+        /// Removes the clipboard format listener.
+        /// </summary>
+        /// <returns><c>true</c>, if clipboard format listener was removed, <c>false</c> otherwise.</returns>
+        /// <param name="hwnd">The handle.</param>c
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+        /// <summary>
+        /// Registers the hot key.
+        /// </summary>
+        /// <returns><c>true</c>, if hot key was registered, <c>false</c> otherwise.</returns>
+        /// <param name="handle">The window handle.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="modifiers">The modifiers.</param>
+        /// <param name="vk">The virtual key.</param>
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr handle, int id, int modifiers, int vk);
+
+        /// <summary>
+        /// Handles the pause resume button click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnPauseResumeButtonClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the minimize tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnMinimizeToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the delete selected button click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnDeleteSelectedButtonClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the clear button click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnClearButtonClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the new tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnNewToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the exit tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the always on top tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnAlwaysOnTopToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the hide close button tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnHideCloseButtonToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the daily releases public domain dailycom tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnDailyReleasesPublicDomainDailycomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the original thread donation codercom tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the source code githubcom tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the about tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the main form load event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnMainFormLoad(object sender, EventArgs e)
+        {
+            // TODO Add code
+        }
+
+        /// <summary>
+        /// Handles the main form form closing event.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void OnMainFormFormClosing(object sender, FormClosingEventArgs e)
+        {
+            // TODO Add code
+        }
+    }
+}
---
+++ ClipboardFIFO/SettingsData.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="SettingsData.cs" company="PublicDomain.com">
+//     CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication
+//     https://creativecommons.org/publicdomain/zero/1.0/legalcode
+// </copyright>
+
+namespace ClipboardFIFO
+{
+    // Directives
+    using System;
+    using System.Drawing;
+
+    /// <summary>
+    /// Settings data.
+    /// </summary>
+    public class SettingsData
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ClipboardFIFO.SettingsData"/> class.
+        /// </summary>
+        public SettingsData()
+        {
+            // Parameterless constructor for serialization to work
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:ClipboardUrlSaver.SettingsData"/> is always on top.
+        /// </summary>
+        /// <value><c>true</c> if always on top; otherwise, <c>false</c>.</value>
+        public bool AlwaysOnTop { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:MultilingualWordCounter.SettingsData"/> hides the close button.
+        /// </summary>
+        /// <value><c>true</c> if hide close button; otherwise, <c>false</c>.</value>
+        public bool HideCloseButton { get; set; } = false;
+    }
+}
